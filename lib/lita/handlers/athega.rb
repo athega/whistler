2a7c1be5479fce63e144a22d83fccce11ba2a106
Peter Hellberg <peter@c7.se>
2013-08-25 18:05:10 +0200

    Added support for XKCD

--- lib/lita/handlers/athega.rb
+++ lib/lita/handlers/athega.rb
@@ -4,17 +4,13 @@ module Lita
   module Handlers
     class Athega < Handler
       # coderwall <coderwall username> - Returns coder achievements from coderwall.com
-      # haters - Returns a random haters gonna hate url
       # hn.top - refer to the top item on hn
       # hn[i] - refer to the ith item on hn
       # hubot 9gag me - Returns a random meme image
       # hubot <keyword> tweet - Returns a link to a tweet about <keyword>
       # hubot abstract <topic> - Prints a nice abstract of the given topic
-      # hubot animate me <query> - The same thing as `image me`, except adds a few parameters to try to return an animated GIF instead.
       #   hubot eval me <lang> <code> - evaluate <code> and show the result
-      # hubot gem whois <gemname> - returns gem details if it exists
       # hubot hn top <N> - get the top N items on hacker news (or your favorite RSS feed)
-      # hubot image me <query> - The Original. Queries Google Images for <query> and returns a random top result.
       #   hubot md5|sha|sha1|sha256|sha512|rmd160 me <string> - Generate hash of <string>
       # hubot mustache me <query> - Searches Google Images for the specified query and mustaches it.
       #   hubot mustache me <url> - Adds a mustache to the specified URL.
@@ -22,14 +18,11 @@ module Lita
       # hubot roll <x>d<y> - roll x dice, each of which has y sides
       # hubot roll dice - Roll two six-sided dice
       # hubot ruby|rb <script> - Evaluate one line of Ruby script
-      # hubot there's a gem for <that> - Returns a link to a gem on rubygems.org
-      # hubot xkcd - The latest XKCD comic
-      # hubot xkcd <num> - XKCD comic <num>
       # ship it - Display a motivation squirrel
       # yoda pic - Returns a random quote
       # yoda quote - Returns a random yoda quote
-
-
+      # hubot xkcd - The latest XKCD comic
+      # hubot xkcd <num> - XKCD comic <num>
 
       route /^\/athegian\s+(\w+)/, :athegian,
         help: { "/athegian NAME" => "Retrieves image and position for employee" }
@@ -41,6 +34,25 @@ module Lita
           response.reply(data['medium_image_url'])
           response.reply(data['name'])
           response.reply(data['position'])
+        else
+          response.reply("Sorry, no such employee at Athega.")
+        end
+      end
+
+      route /^\/xkcd\s?(\d+)?/, :xkcd, help: {
+        "/xkcd"     => "The latest XKCD comic",
+        "/xkcd NUM" => "XKCD comic <num>"
+      }
+
+      def xkcd(response)
+        num = response.matches[0][0]
+
+        data = num ? get_xkcd_data(num) : get_latest_xkcd_data
+
+        if data
+          response.reply(data['img'])
+          response.reply(data['title'])
+          response.reply(data['alt'])
         end
       end
 
@@ -66,7 +78,12 @@ module Lita
 
         response.write(html)
       end
+
     private
+
+      def get_latest_xkcd_data
+        json_get('http://xkcd.com/info.0.json')
+      end
 
       def get_random_reddit_field(path, field)
         url = reddit_url(path)
--- spec/lita/handlers/athega_spec.rb
+++ spec/lita/handlers/athega_spec.rb
@@ -31,6 +31,39 @@ describe Lita::Handlers::Athega, lita_handler: true do
     end
   end
 
+  it { routes("/xkcd foo").to(:xkcd) }
+
+  describe "xkcd" do
+    it "returns the latest xkcd comic by default" do
+      Lita::Handlers::Athega.any_instance.
+        should_receive(:get_latest_xkcd_data).
+        and_return({
+          'img'   => 'foo',
+          'title' => 'bar',
+          'alt'   => 'baz'
+        })
+
+      send_message('/xkcd')
+
+      replies.should == ['foo', 'bar', 'baz']
+    end
+
+    it "returns the specified xkcd comic" do
+      Lita::Handlers::Athega.any_instance.
+        should_receive(:get_xkcd_data).
+        with("123").
+        and_return({
+          'img'   => '123_foo',
+          'title' => '123_bar',
+          'alt'   => '123_baz'
+        })
+
+      send_message('/xkcd 123')
+
+      replies.should == ['123_foo', '123_bar', '123_baz']
+    end
+  end
+
   it { routes("/aww").to(:aww) }
 
   describe "aww" do
