8b227036cc32b1093f82f06919cac459df14f9ef
Peter Hellberg <peter@c7.se>
2014-11-22 12:06:14 +0100

    Added missing comments and cleaned up statements

--- robots/decide.go
+++ robots/decide.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// DecideBot decides, deal with it
 type DecideBot struct {
 }
 
@@ -14,16 +15,18 @@ func init() {
 	RegisterRobot("decide", func() (robot Robot) { return new(DecideBot) })
 }
 
+// Run executes a deferred action
 func (d DecideBot) Run(command *SlashCommand) (slashCommandImmediateReturn string) {
 	go d.DeferredAction(command)
 
 	if strings.TrimSpace(command.Text) == "" {
 		return "I need something to decide on!"
-	} else {
-		return ""
 	}
+
+	return ""
 }
 
+// DeferredAction makes two incoming webhook calls
 func (d DecideBot) DeferredAction(command *SlashCommand) {
 	response := new(IncomingWebhook)
 
@@ -43,22 +46,23 @@ func (d DecideBot) DeferredAction(command *SlashCommand) {
 	}
 }
 
+// Description describes what the robot does
 func (d DecideBot) Description() (description string) {
 	return strings.Join([]string{
 		"Decides your fate!",
-		"Usage: /decide Life Death ...",
+		"Usage: /whistler decide Life, Death, ...",
 		"Expected Response: Deciding on (Life, Death, ...)",
-		"Decided on Life!",
+		"Decided on Life",
 	}, "\n\t")
 }
 
+// Decide is the implementation of the decision logic
 func Decide(Fates []string) (result string) {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	n := len(Fates)
+	r, n := rand.New(rand.NewSource(time.Now().UnixNano())), len(Fates)
 
 	if n > 0 {
 		return strings.TrimSpace(Fates[r.Intn(n)])
-	} else {
-		return fmt.Sprintf("Error")
 	}
+
+	return fmt.Sprintf("Error")
 }
